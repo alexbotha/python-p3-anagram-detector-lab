510e580608878559ddedc78d37c58688a62746f7
alexbotha <alexbotha@hotmail.co.uk>
2023-09-21 11:09:41 +0100

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,19 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        
+        self.sorted_word = sorted([letter for letter in word])
+
+    def match(self, anagram):
+        x = []
+
+        for word in anagram:
+            if sorted([letter for letter in word]) == self.sorted_word:
+                x.append(word)
+
+        return x
+
+
+
+  
+
+
